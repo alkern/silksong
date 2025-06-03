b630eb03107f8c5d23a1dad05c5559c54d53e8ff
alexander <al-kern@web.de>
2025-06-03 20:22:38 +0200

    feat: run as fullscreen
    Signed-off-by: alexander <al-kern@web.de>

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,33 @@ use crate::state::GameStatePlugin;
 use crate::visuals::VisualsPlugin;
 use bevy::asset::AssetMetaCheck;
 use bevy::prelude::*;
+use bevy::window::WindowMode;
 
 fn main() {
     App::new()
         // Bevy plugins
-        .add_plugins(DefaultPlugins.set(AssetPlugin {
-            // Wasm builds will check for meta files (that don't exist) if this isn't set.
-            // This causes errors and even panics in web builds on itch.
-            // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
-            meta_check: AssetMetaCheck::Never,
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(AssetPlugin {
+                    // Wasm builds will check for meta files (that don't exist) if this isn't set.
+                    // This causes errors and even panics in web builds on itch.
+                    // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+                    meta_check: AssetMetaCheck::Never,
+                    ..default()
+                })
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Silksong".into(),
+                        name: Some("Silksong".into()),
+                        mode: WindowMode::Fullscreen(
+                            MonitorSelection::Primary,
+                            VideoModeSelection::Current,
+                        ),
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
         // external plugins
         .add_plugins(bevy_svg::prelude::SvgPlugin)
         // game plugins
