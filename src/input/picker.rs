6ab09e0bf78a80fad238a0e60e57524e36dd8a10
alexander <al-kern@web.de>
2025-06-04 19:44:18 +0200

    feat: placement of notes and triggers
    Signed-off-by: alexander <al-kern@web.de>

--- src/input/picker.rs
+++ src/input/picker.rs
@@ -1,6 +1,10 @@
+use crate::core::game::CoreAssets;
+use crate::core::model::Note;
 use crate::state::{GameState, MinimalGameState};
 use bevy::color::palettes::basic::WHITE;
+use bevy::input::mouse::MouseButtonInput;
 use bevy::prelude::*;
+use bevy_svg::prelude::{Origin, Svg2d};
 
 pub(super) struct PickerPlugin;
 
@@ -9,7 +13,8 @@ impl Plugin for PickerPlugin {
         app.add_systems(OnEnter(GameState::SetupGameObjects), setup)
             .add_systems(
                 Update,
-                handle_input.run_if(in_state(MinimalGameState::Running)),
+                (handle_item_switch_input, handle_item_placement)
+                    .run_if(in_state(MinimalGameState::Running)),
             );
     }
 }
@@ -17,21 +22,21 @@ impl Plugin for PickerPlugin {
 #[derive(Component, PartialEq, Debug)]
 enum SelectedItem {
     Trigger,
-    Node,
+    Note,
 }
 
 impl SelectedItem {
     fn switch(&self) -> SelectedItem {
         match self {
-            SelectedItem::Trigger => SelectedItem::Node,
-            SelectedItem::Node => SelectedItem::Trigger,
+            SelectedItem::Trigger => SelectedItem::Note,
+            SelectedItem::Note => SelectedItem::Trigger,
         }
     }
 
     fn name(&self) -> String {
         match self {
             SelectedItem::Trigger => "Trigger".to_string(),
-            SelectedItem::Node => "Node".to_string(),
+            SelectedItem::Note => "Node".to_string(),
         }
     }
 }
@@ -85,7 +90,7 @@ fn setup(mut commands: Commands) {
         });
 }
 
-fn handle_input(
+fn handle_item_switch_input(
     mut commands: Commands,
     mut ui: Query<(Entity, &SelectedItem, &mut Text)>,
     input: Res<ButtonInput<KeyCode>>,
@@ -95,6 +100,55 @@ fn handle_input(
             let next_item = item.switch();
             text.0 = next_item.name();
             commands.entity(entity).insert(next_item);
+        }
+    }
+}
+
+fn handle_item_placement(
+    mut mouse_button_input_events: EventReader<MouseButtonInput>,
+    window: Query<&Window>,
+    camera: Query<(&Camera, &GlobalTransform)>,
+    mut commands: Commands,
+    selected_item: Query<&SelectedItem>,
+    assets: Res<CoreAssets>,
+) {
+    for event in mouse_button_input_events.read() {
+        if event.button == MouseButton::Left {
+            // calculation taken from https://bevy-cheatbook.github.io/cookbook/cursor2world.html
+            let (camera, camera_transform) = camera.single().expect("Camera must exist");
+
+            if let Some(world_position) = window
+                .get(event.window)
+                .expect("Window must exist")
+                .cursor_position()
+                .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor).ok())
+                .map(|ray| ray.origin.truncate())
+            {
+                let item = selected_item.single().expect("SelectedItem must exist");
+
+                match item {
+                    SelectedItem::Trigger => {
+                        commands.spawn((
+                            Name::new(item.name()),
+                            crate::core::model::Trigger::default(),
+                            Transform::from_translation(world_position.extend(0.0))
+                                .with_scale(Vec3::splat(0.05)),
+                            Svg2d(assets.trigger_icon_play.clone()),
+                            Origin::Center,
+                        ));
+                    }
+                    SelectedItem::Note => {
+                        commands.spawn((
+                            Name::new(item.name()),
+                            Note,
+                            Transform::from_translation(world_position.extend(0.0))
+                                .with_scale(Vec3::splat(0.025)),
+                            Svg2d(assets.note_icon.clone()),
+                            Origin::Center,
+                        ));
+                    }
+                }
+            }
         }
     }
 }
